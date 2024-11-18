e621a624a143f0c1ea45758b70d883313b402264
Álvaro Holguera <correo@laultimapregunta.com>
2024-11-18 12:42:30 +0100

    Inputs

--- Pruebas_DEIM-M-24/Assets/Scripts/PlayerManager.cs
+++ Pruebas_DEIM-M-24/Assets/Scripts/PlayerManager.cs
@@ -7,25 +7,68 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] int lifes = 4;
     public float shield;
     bool alive;
-    float damage;
+   // float damage;
     bool powerUp;
+
+    //Variables de inicio de juego
+    int vidasIniciales = 4;
+    float escudoInicial = 100f;
 
     private void Start()
     {
-        lifes = 4;
-        shield = 100f;
+        StarGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectarInputs();
+
+    }
+
+    void DetectarInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && alive == true)
+        {
+            Impact();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape) && alive == false)
+        {
+            StarGame();
+
+        }
+    }
+
+    void StarGame()
+    {
+        lifes = vidasIniciales;
+        shield = escudoInicial;
         alive = true;
         powerUp = false;
-        damage = 25f;
+        print("Comienza el juego!");
+    }
+
+    float CalcularDamage()
+    {
+        //damage = 25f;
+        //Calculo el daño al azar
+        float x = Random.Range(10f, 60f);
+        x = Mathf.Round(x * 10)/10;
+        return x;
     }
 
     void Impact()
     {
+        //Declaro una variable local
+        float damage;
+        //Le doy el valor que me retorna el método de calcular daño
+        damage = CalcularDamage();
         print("Has recibido un impacto con un daño de " + damage);
         shield -= damage;
         if(shield < 0)
         {
-            lifes--;
+            RestarVida();
         }
         else
         {
@@ -35,14 +78,34 @@ public class PlayerManager : MonoBehaviour
         //print("te queda de escudo " + shield);
     } 
 
-    // Update is called once per frame
-    void Update()
+    void RestarVida()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        lifes--;
+        if(lifes <= 0)
         {
-            Impact();
+            Die();
         }
+        else
+        {
+            shield = escudoInicial;
+            print("Te han quitado una vida. Te quedan " + lifes);
+            BuclePorLaVida();
+        }
+    }
 
+    void BuclePorLaVida()
+    {
+        for(int i = 0; i < lifes; i++)
+        {
+            print("Bucle por la vida " + i);
+
+        }
+    }
+
+    void Die()
+    {
+        print("Estás muerto. Pulsa Escape para empezar de nuevo");
+        alive = false;
     }
 
 
